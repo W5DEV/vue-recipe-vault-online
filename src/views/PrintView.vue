91553696605ea78cdee17343e0a3def368551429
John Larson <jl@buildmidwestern.com>
2022-11-12 20:34:59 -0600

    updated printview page

--- src/views/PrintView.vue
+++ src/views/PrintView.vue
@@ -4,8 +4,8 @@
     <div class="flex flex-row justify-between items-start w-full mb-8">
       <div class="flex flex-col justify-center items-start w-1/2">
         <div class="flex flex-col justify-center items-center">
-          <LogoColor class="h-14 w-14 m-4" />
-          <span class="text-primary font-fredericka font-bold text-2xl">{{
+          <LogoBlack class="h-14 w-14 m-4" />
+          <span class="font-fredericka text-black font-bold text-2xl">{{
             siteName
           }}</span>
         </div>
@@ -13,15 +13,16 @@
       <div class="pt-4 pr-4 flex flex-row justify-end items-center w-1/2">
         <div @click="printPage()">
           <PrinterIcon
-            class="text-primary h-10 w-10 m-2 cursor-pointer"
+            class="text-gray-400 h-10 w-10 m-2 cursor-pointer"
             aria-hidden="true" />
         </div>
-        <RouterLink to="/dashboard">
-          <HomeIcon class="text-primary h-10 w-10 m-2" aria-hidden="true" />
-        </RouterLink>
+        <HomeIcon
+          class="text-gray-400 h-10 w-10 m-2 cursor-pointer"
+          @click="$router.go(-1)"
+          aria-hidden="true" />
       </div>
     </div>
-    <div class="text-3xl leading-6 text-gray-900 font-bold">
+    <div class="text-3xl leading-6 text-gray-800 font-bold">
       {{ recipe.title }}
       <span class="font-normal text-sm italic ml-6">
         Chef: {{ recipe.profiles.username }}</span
@@ -30,7 +31,7 @@
     <p class="text-base text-gray-500 my-4">Category: {{ recipe.category }}</p>
     <p class="text-base text-gray-500">Description: {{ recipe.description }}</p>
     <div class="mt-8">
-      <span class="font-bold text-2xl">Ingredients:</span>
+      <span class="font-bold text-2xl text-gray-800">Ingredients:</span>
       <span
         v-for="ingredient in recipe.ingredients.sort((a, b) =>
           a.id > b.id ? 1 : -1
@@ -48,7 +49,7 @@
       </span>
     </div>
     <div class="my-8">
-      <span class="font-bold text-2xl">Instructions:</span>
+      <span class="font-bold text-2xl text-gray-800">Instructions:</span>
       <span
         class="flex flex-col justify-center items-start"
         v-for="instruction in recipe.instructions.sort((a, b) =>
@@ -78,8 +79,7 @@
 <script setup>
 import { useRecipeStore, useSiteStore } from '../stores/base';
 import { HomeIcon, PrinterIcon } from '@heroicons/vue/24/outline';
-import { RouterLink } from 'vue-router';
-import LogoColor from '../assets/cookbook-color.svg?component';
+import LogoBlack from '../assets/cookbook-black.svg?component';
 
 const recipeStore = useRecipeStore();
 const siteStore = useSiteStore();
